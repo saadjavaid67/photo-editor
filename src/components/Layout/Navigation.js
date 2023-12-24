e56233166d8b437d25911ba19db3af12fe4c4015
saadjavaid67 <saadjavaid67@gmail.com>
2023-12-25 03:23:01 +0500

    removed hamburg icon

--- src/components/Layout/Navigation.js
+++ src/components/Layout/Navigation.js
@@ -15,7 +15,7 @@ const Navigation = () => {
                         aria-expanded='false'
                         aria-label='Toggle navigation'
                     >
-                        <span className='navbar-toggler-icon' />
+                        {/* <span className='navbar-toggler-icon' /> */}
                     </button>
                     {/* <div className='collapse navbar-collapse' id='navbarSupportedContent'>
                         <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
